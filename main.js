eada3b7d9436d87e42fd341a15d8226ee88da731
Juan Pablo Sarmiento <60159763+jpsarmiento@users.noreply.github.com>
2021-09-24 14:32:39 -0500

    commit1

---
+++ main.js
@@ -0,0 +1,2 @@
+url = 'https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json';
+fetch(url).then(response => response.json()).then((data) => console.log(data));
